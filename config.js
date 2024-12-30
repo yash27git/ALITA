52ed2f858033dc0d5dabaddc8e892e26d8a7b818
yash27git <singhdimpal1981+git@gmail.com>
2024-12-30 18:01:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ OWNER_NUMBER: process.env.OWNER_NUMBER || "917275383838",
 MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/yash27git/ALITA/refs/heads/main/ALITA%20DEVLOPER%20IMG.jpg", // DO NOT CHANGE
 ALIVE_MSG: process.env.ALIVE_MSG || "ᴀʟɪᴛᴀ ᴀɪ ɪs ᴀʟɪᴠᴇ. ᴋᴇᴇᴘ sᴜᴘᴘᴏʀᴛɪɴɢ *❄️*\n\n> ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴘᴀɴᴅᴇʏ ᴊɪ",
 ANTI_LINK: process.env.ANTI_LINK || "true",
@@ -40,7 +40,7 @@ AUTO_REACT: process.env.AUTO_REACT || "false",
 HEART_REACT: process.env.HEART_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
 BOT_NAME: process.env.BOT_NAME || "『 ＡＬＩＴＡ 』",
-OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+OMDB_API_KEY: process.env.OMDB_API_KEY || "YASH YT FF", // omdbapi.com
 };
 
 //--------------MADE BY PANDEY JI 2024 DEC---------------->-
